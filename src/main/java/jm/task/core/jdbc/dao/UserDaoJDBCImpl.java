365046a7bc7df8dfffda1820b5f20d9776791bab
artsnopok <artemsnopok@yandex.ru>
2021-07-26 15:30:32 +0300

    Исправлены замечания

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,81 +9,154 @@ import java.util.List;
 import java.util.Objects;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
+
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-        String SQL = "CREATE TABLE IF NOT EXISTS users " +
-                "(id BIGINT auto_increment, " +
-                " name VARCHAR(20) not NULL, " +
-                " lastName VARCHAR (20) not NULL, " +
-                " age TINYINT (3) not NULL, " +
-                " PRIMARY KEY (id))";
-        try (Statement statement = Objects.requireNonNull(Util.getNewConnection()).createStatement()) {
-            statement.executeUpdate(SQL);
+        Connection connection = Util.getNewConnection();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users " +
+                    "(id BIGINT auto_increment, " +
+                    " name VARCHAR(20) not NULL, " +
+                    " lastName VARCHAR (20) not NULL, " +
+                    " age TINYINT (3) not NULL, " +
+                    " PRIMARY KEY (id))");
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
             e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
     }
 
     public void dropUsersTable() {
-        String SQL = "DROP TABLE IF EXISTS users";
-        try (Statement statement = Objects.requireNonNull(Util.getNewConnection()).createStatement()) {
-            statement.executeUpdate(SQL);
-            Util.getNewConnection().close();
+        Connection connection = Util.getNewConnection();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS users");
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
             e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String SQL = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
-        try (PreparedStatement pst = Objects.requireNonNull(Util.getNewConnection()).prepareStatement(SQL)) {
+        Connection connection = Util.getNewConnection();
+        try (PreparedStatement pst = connection
+                .prepareStatement("INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)")) {
             pst.setString(1, name);
             pst.setString(2, lastName);
             pst.setByte(3, age);
             pst.execute();
             System.out.println("User с именем – " + name + " добавлен в базу данных.");
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
             e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
     }
 
     public void removeUserById(long id) {
-        String SQL = "DELETE FROM users WHERE id=?";
-        try (PreparedStatement pst = Objects.requireNonNull(Util.getNewConnection()).prepareStatement(SQL)) {
+        Connection connection = Util.getNewConnection();
+        try (PreparedStatement pst = connection
+                .prepareStatement("DELETE FROM users WHERE id=?")) {
             pst.setLong(1, id);
             pst.execute();
             System.out.println("User c id - " + id + " удален.");
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
             e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
 
     }
 
     public List<User> getAllUsers() {
-        String SQL = "SELECT * FROM users";
+        Connection connection = Util.getNewConnection();
         List<User> list = new ArrayList<>();
-        try (Statement statement = Objects.requireNonNull(Util.getNewConnection()).createStatement();
-             ResultSet rs = statement.executeQuery(SQL);) {
+        try (Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery("SELECT * FROM users")) {
             while (rs.next()) {
-                User user = new User(rs.getString(2), rs.getString(3), rs.getByte(4));
-                list.add(user);
-                user.setId(rs.getLong(1));
+                list.add(new User(rs.getLong(1), rs.getString(2),
+                        rs.getString(3), rs.getByte(4)));
             }
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
             e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
         return list;
     }
 
     public void cleanUsersTable() {
-        String SQL = "DELETE FROM users";
-        try (Statement statement = Objects.requireNonNull(Util.getNewConnection()).createStatement()) {
-            statement.executeUpdate(SQL);
+        Connection connection = Util.getNewConnection();
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("TRUNCATE TABLE users");
+            connection.commit();
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
             e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,14 +6,17 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydb?useSSL=false";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "e19720q11q";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static Connection connection = null;
 
     public static Connection getNewConnection() {
-        Connection connection;
-        try  {
-            return connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection.setAutoCommit(false);
+        } catch (SQLException | ClassNotFoundException e) {
             System.out.println("Соединение с БД не установлено");
         }
-        return null;
+        return connection;
     }
 }
